8b357279307fe7c077e8ecf0099803b9f91f9d01
Jean Barp <85322647+jeanbarp@users.noreply.github.com>
2023-05-25 14:08:32 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+function tocaSom (selectorAudio){
+    document.querySelector(selectorAudio).play();   
+
+    if(elemento != null && elemento.localName ==='audio') {
+        elemento.play();   
+    }
+    
+    else{
+        alert('Elemento não encontrado');
+    }
+}
+
+
+const listadeTeclas = document.querySelectorAll('.tecla');
+
+
+for (let contador = 0; contador < listadeTeclas.length; contador++){
+
+    const tecla = listadeTeclas[contador];
+
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function (){
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento){
+
+
+
+        if(evento.code === 'Space' || evento.code === 'Enter') {
+        tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }    
+}
